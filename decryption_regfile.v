98bfa35b896219d881ca0e38f75469b5c89bd182
Anton Mircea-Pavel <mircea_pavel.anton@stud.acs.upb.ro>
2020-12-03 20:20:46 +0200

    Add doc comments

--- decryption_regfile.v
+++ decryption_regfile.v
@@ -1,46 +1,49 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company:		A.C.S
+// Engineer:	Anton Mircea-Pavel
 // 
-// Create Date:    20:13:49 11/23/2020 
-// Design Name: 
-// Module Name:    decryption_regfile 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
+// Create Date:		23:12:00 11/23/2020 
+// Design Name:
+// Module Name:		Register Bank
+// Project Name:	Tema2_Decryption
+// Target Devices:	N/A
+// Tool versions:	N/A
+// Description:		This block is responsible for handling the input data from the outside.
+//					It stores the keys for the cyphers and sends the select signals to the
+//					*MUX blocks
 //
-// Dependencies: 
+// Dependencies:	N/A
 //
-// Revision: 
+// Revision:
 // Revision 0.01 - File Created
-// Additional Comments: 
-//
+// Revision 0.02 - Doc Comments Added
 //////////////////////////////////////////////////////////////////////////////////
+
 module decryption_regfile #(
-			parameter addr_witdth = 8,
-			parameter reg_width 	 = 16
+			parameter addr_witdth	= 8,
+			parameter reg_width		= 16
 		)(
 			// Clock and reset interface
-			input clk, 
-			input rst_n,
+			input clk,		// system clock
+			input rst_n,	// reset signal
 			
 			// Register access interface
-			input[addr_witdth - 1:0] addr,
-			input read,
-			input write,
-			input [reg_width -1 : 0] wdata,
-			output reg [reg_width -1 : 0] rdata,
-			output reg done,
-			output reg error,
+			input[addr_witdth - 1:0]		addr, // the address of the desired register
+			input							read, // basically a read_enable signal
+			input							write, // basically a write_enable signal
+			input [reg_width -1 : 0]		wdata, // the written data
+			output reg [reg_width -1 : 0]	rdata, // the read data
+			output reg						done, // 'bool' value to indicate status
+			output reg						error, // 'bool' value to indicate errors
 			
 			// Output wires
-			output reg[reg_width - 1 : 0] select,
+			output reg[reg_width - 1 : 0] select,	// The signal that will be 
+													//sent to the MUX & DEMUX blocks
 			output reg[reg_width - 1 : 0] caesar_key,
 			output reg[reg_width - 1 : 0] scytale_key,
 			output reg[reg_width - 1 : 0] zigzag_key
-    );
+	);
 
 // TODO implementati bancul de registre.
 	

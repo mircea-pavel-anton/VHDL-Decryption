bbc62c427aa1c338b5abd249f9dbb44561c091b9
Anton Mircea-Pavel <mircea_pavel.anton@stud.acs.upb.ro>
2020-12-03 23:01:03 +0200

    Remove intermediary signals and doc revisions

--- mux.v
+++ mux.v
@@ -17,6 +17,9 @@
 // Revision:
 // Revision 0.01 - File Created
 // Revision 0.02 - Doc Comments Added
+// Revision 0.03 - General Logic Explained in Comments
+// Revision 0.04 - First attempt at an implementation
+// Revision 0.05 - Bug fix: remove intermediary signals and work directly with data_o and valid_o
 //////////////////////////////////////////////////////////////////////////////////
 
 module mux #(
@@ -41,7 +44,7 @@ module mux #(
 		input						valid1_i,	// Scytale Dec. Output Enable
 
 		input [D_WIDTH - 1 : 0]		data2_i,	// ZigZag Dec. Output Data
-		input						valid2_i,	// ZigZag Dec. Output Enable
+		input						valid2_i	// ZigZag Dec. Output Enable
 	);
 	/////////////////////////// LOGIC OVERVIEW ///////////////////////////
 	//	Everythin happens on the positive edge of the [clk] signal		//
@@ -54,59 +57,52 @@ module mux #(
 	//		set [valid_o] to LOW										//
 	//////////////////////////////////////////////////////////////////////
 
-	
-	output reg [D_WIDTH - 1 : 0]	stored_data;
-	output reg						valid_sig;
-
 	always @(posedge clk) begin
 		// If the reset signal is HIGH, disable all outs
 		if (rst_n) begin
-			stored_data <= 0;
-			valid_sig <= 0;
+			data_o <= 0;
+			valid_o <= 0;
 		end else begin // if reset is LOW
 			case (select)
-				2b'00: begin
+				2'b00: begin
 					// This statement handles the 'first' clock cycle, starting
 					// the count when the [validX_i] gets HIGH.
 					// What happens here is that the ternary operator assigns
-					// the value of [dataX_i] to [stored_data] when [validX_i]
+					// the value of [dataX_i] to [data_o] when [validX_i]
 					// is HIGH, and 0 otherwise
-					stored_data <= (valid0_i == 1) ? data0_i : 0;
+					data_o <= (valid0_i == 1) ? data0_i : 0;
 
 					// This statement handles the 'second' and 'third' 
 					// clock cycles, starting the count when the [validX_i]
 					// gets HIGH.
 					// What happens here is that the ternary operator assigns
 					// the boolean value of evaluating the expression
-					// (valid0_i == 1 && valid_sig == 0) to valid_sig
+					// (valid0_i == 1 && valid_o == 0) to valid_o
 					// What this basically does is that when [validX_i] gets
-					// HIGH and [valid_sig] is LOW, it toggles it from
+					// HIGH and [valid_o] is LOW, it toggles it from
 					// LOW to HIGH and then back from HIGH to LOW in
 					// 2 clock cycles
-					valid_sig <= (valid0_i == 1 && valid_sig == 0);
+					valid_o <= (valid0_i == 1 && valid_o == 0) ? 1 : 0;
 				end
 				
-				2b'01: begin // see comments above
-					stored_data <= (valid1_i == 1) ? data1_i : 0;
-					valid_sig <= (valid1_i == 1 && valid_sig == 0);
+				2'b01: begin // see comments above
+					data_o <= (valid1_i == 1) ? data1_i : 0;
+					valid_o <= (valid1_i == 1 && valid_o == 0) ? 1 : 0;
 				end
 				
-				2b'10: begin // see comments above
-					stored_data <= (valid2_i == 1) ? data2_i : 0;
-					valid_sig <= (valid2_i == 1 && valid_sig == 0);
+				2'b10: begin // see comments above
+					data_o <= (valid2_i == 1) ? data2_i : 0;
+					valid_o <= (valid2_i == 1 && valid_o == 0) ? 1 : 0;
 				end
 				
 				// The [select] signal should never reach this case, but we're
 				// implementing it just to be safe.
 				// If [select] has an invalid value, then disable all outs
-				2b'11: begin
+				2'b11: begin
 					data_o <= 0;
 					valid_o <= 0;
 				end
 			endcase
 		end
 	end
-	
-	assign data_o = stored_data;
-	assign valid_o = valid_sig;
 endmodule

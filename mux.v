007b779b246f1aacfb87be41a4c5a5ab93fc872f
Anton Mircea-Pavel <mircea_pavel.anton@stud.acs.upb.ro>
2020-12-03 16:08:12 +0200

    Add doc comments

--- mux.v
+++ mux.v
@@ -1,47 +1,48 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company:		A.C.S
+// Engineer:	Anton Mircea-Pavel
 // 
-// Create Date:    20:53:30 11/26/2020 
-// Design Name: 
-// Module Name:    mux 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
+// Create Date:		23:12:00 11/23/2020 
+// Design Name:
+// Module Name:		demux
+// Project Name:	Tema2_Decryption
+// Target Devices:	N/A
+// Tool versions:	N/A
+// Description:		Mux block that routes the decrypted message from a decryptor 
+//					block to the output of the system
 //
-// Dependencies: 
+// Dependencies:	N/A
 //
-// Revision: 
+// Revision:
 // Revision 0.01 - File Created
-// Additional Comments: 
-//
+// Revision 0.02 - Doc Comments Added
 //////////////////////////////////////////////////////////////////////////////////
+
 module mux #(
 		parameter D_WIDTH = 8
 	)(
 		// Clock and reset interface
-		input clk,
-		input rst_n,
+		input clk,		// system clock
+		input rst_n,	// reset signal
 		
 		//Select interface
 		input[1:0] select,
 		
 		// Output interface
-		output reg[D_WIDTH - 1 : 0] data_o,
-		output reg						 valid_o,
-				
+		output reg[D_WIDTH - 1 : 0]	data_o,		// System Output Data
+		output reg					valid_o,	// System Output Enable
+
 		//output interfaces
-		input [D_WIDTH - 1 : 0] 	data0_i,
-		input   							valid0_i,
-		
-		input [D_WIDTH - 1 : 0] 	data1_i,
-		input   							valid1_i,
-		
-		input [D_WIDTH - 1 : 0] 	data2_i,
-		input     						valid2_i
-    );
+		input [D_WIDTH - 1 : 0]		data0_i,	// Caesar Dec. Output Data
+		input						valid0_i,	// Caesar Dec. Output Enable
+
+		input [D_WIDTH - 1 : 0]		data1_i,	// Scytale Dec. Output Data
+		input						valid1_i,	// Scytale Dec. Output Enable
+
+		input [D_WIDTH - 1 : 0]		data2_i,	// ZigZag Dec. Output Data
+		input						valid2_i,	// ZigZag Dec. Output Enable
+	);
 	
 	//TODO: Implement MUX logic here
 

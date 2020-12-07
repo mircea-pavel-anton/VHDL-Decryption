f79f95acf5204ec7a96ae0555db3630a3aede859
Anton Mircea-Pavel <mircea_pavel.anton@stud.acs.upb.ro>
2020-12-07 10:59:17 +0200

    Add code doc

--- caesar_decryption.v
+++ caesar_decryption.v
@@ -20,6 +20,7 @@
 // Revision 0.04 - General Logic Exmplained in top comment
 // Revision 0.05 - First attempt at an implementation
 // Revision 0.06 - Rephrase explanation to be more verbose
+// Revision 0.07 - Add comments throughout the code
 //////////////////////////////////////////////////////////////////////////////////
 module caesar_decryption #(
 				parameter D_WIDTH = 8,
@@ -56,13 +57,25 @@ module caesar_decryption #(
 	//////////////////////////////////////////////////////////////////////
 
 	always @(posedge clk) begin
+		// busy should always be 0, as per the documentation
 		busy <= 0;
+		
+		// if !reset is high -> reset is low
 		if (rst_n) begin
+			// set [valid_o] to high if [valid_i] was high last clock
 			valid_o <= valid_i;
+
+			// if [valid_i] was high last clock, decrypt input message and
+			// send it to [data_o]
 			data_o <= (valid_i) ? data_i - key : 0;
-			if (valid_o)
-				$display("| key\t| in\t| out\t|\n|0x%0h\t| 0x%0h\t| 0x%0h\t|", key, data_i, data_o);
-		end else begin
+
+			// DEBUG
+			//if (valid_o)
+			//	$display("| key\t| in\t| out\t|\n|0x%0h\t| 0x%0h\t| 0x%0h\t|", key, data_i, data_o);
+		end 
+		
+		// if reset is high, set [valid_o] to low. [data_o] CAN be ignored
+		else begin
 			valid_o <= 0;
 			data_o <= 0;
 		end

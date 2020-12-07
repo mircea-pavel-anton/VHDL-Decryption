fd8c6ae26273b2251809904298697f4b6cdfc8ef
Anton Mircea-Pavel <mircea_pavel.anton@stud.acs.upb.ro>
2020-12-07 14:07:15 +0200

    First attempt at an implementation

--- scytale_decryption.v
+++ scytale_decryption.v
@@ -16,6 +16,7 @@
 // Revision: 
 // Revision 0.01 - File Created
 // Revision 0.02 - Doc Comments Added
+// Revision 0.03 - First attempt at an implementation
 //////////////////////////////////////////////////////////////////////////////////
 module scytale_decryption#(
 			parameter D_WIDTH = 8, 
@@ -38,11 +39,57 @@ module scytale_decryption#(
 			// Output interface
 			output reg busy,					// Indicates processing is taking place
 			output reg[D_WIDTH - 1:0] data_o,	// The decrypted message
-			output reg valid_o,					// Output enable
-    );
+			output reg valid_o					// Output enable
+	);	
 
-// TODO: Implement Scytale Decryption here
+	reg [D_WIDTH * MAX_NOF_CHARS - 1 : 0] message = 0;
+	reg [KEY_WIDTH - 1 : 0] i = 0;
+	reg [KEY_WIDTH - 1 : 0] j = 0;
+	reg [KEY_WIDTH - 1 : 0] k = 0;
 
+	always @(posedge clk) begin
+		if (rst_n) begin
+			if (valid_i) begin
+				if (data_i != START_DECRYPTION_TOKEN) begin
+					message[D_WIDTH * i +: D_WIDTH ] = data_i;
+					i = i + 1;
+				end else begin
+					j <= 0;
+					k <= j;
+					busy <= 1;
+					$display("\n| i\t\t| j\t\t| k\t\t| data_o|");
+				end
+			end
 
+			if (busy) begin
+				if (k < i) begin
+					valid_o <= 1;
+					data_o <= message[D_WIDTH * k +: D_WIDTH ];
+					k <= k + key_N;
+					$display("| %d\t| %d\t| %d\t| %s\t\t| message(%d)", i, j, k, data_o, k);
+				end else begin
+					j <= j + 1;
+					k <= j + 1 + key_N;
+					
+					if (j + 1 < key_N) begin
+						data_o <= message[D_WIDTH * (j+1) +: D_WIDTH ];
+						$display("| %d\t| %d\t| %d\t| %s\t\t| message(%d)", i, j, k, data_o, j+1);
+					end else begin
+						i <= 0; j <= 0; k <= 0;
+						message <= 0;
+						valid_o <= 0;
+						data_o <= 0;
+						busy <= 0;
+					end
+				end
+			end
 
+		end else begin
+			i <= 0; j <= 0; k <= 0;
+			message <= 0;
+			valid_o <= 0;
+			data_o <= 0;
+			busy <= 0;
+		end
+	end
 endmodule

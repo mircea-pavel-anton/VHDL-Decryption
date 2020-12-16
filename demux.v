c26723fac5a64038d2276dff9a2069f08c0be9f9
Anton Mircea-Pavel <mircea_pavel.anton@stud.acs.upb.ro>
2020-12-16 16:55:24 +0200

    Migrate to a 4 state FSM structure

--- demux.v
+++ demux.v
@@ -24,35 +24,38 @@
 // Revision 0.07 - Refactor:
 //                  - Switch to ternary operators instead of case statement
 //                  - Refactor if statements for better readability
-// Rvision 0.08 - Complete rework, but data_o is 1cc late...
+// Revision 0.08 - Complete rework, but data_o is 1cc late...
+// Revision 0.09 - Migrate to a 4 state FSM structure
+//               - ( i tried to reverse engineer the signals from the ref modul
+//                 e by looking at the waves)
 //////////////////////////////////////////////////////////////////////////////////
 
 module demux #(
-		parameter MST_DWIDTH = 32,
-		parameter SYS_DWIDTH = 8
-	)(
-		// Clock and reset interface
-		input clk_sys,      // system clock
-		input clk_mst,      // master clock (4x system clock speed)
-		input rst_n,        // negated reset signal
-		
-		//Select interface
-		input[1:0] select,
-		
-		// Input interface
-		input [MST_DWIDTH - 1 : 0]          data_i,     // Encrypted Message
-		input                               valid_i,    // Enable Signal
-		
-		//output interfaces
-		output reg [SYS_DWIDTH - 1 : 0]     data0_o,    // Caesar Dec. Input
-		output reg                          valid0_o,   // Caesar Dec. Enable
-		
-		output reg [SYS_DWIDTH - 1 : 0]     data1_o,    // Scytale Dec. Output
-		output reg                          valid1_o,   // Scytale Dec. Enable
-		
-		output reg [SYS_DWIDTH - 1 : 0]     data2_o,    // ZigZag Dec. Output
-		output reg                          valid2_o    // ZigZag Dec. Enable
-	);
+        parameter MST_DWIDTH = 32,
+        parameter SYS_DWIDTH = 8
+    )(
+        // Clock and reset interface
+        input clk_sys,      // system clock
+        input clk_mst,      // master clock (4x system clock speed)
+        input rst_n,        // negated reset signal
+        
+        //Select interface
+        input[1:0] select,
+        
+        // Input interface
+        input [MST_DWIDTH - 1 : 0]          data_i,     // Encrypted Message
+        input                               valid_i,    // Enable Signal
+        
+        //output interfaces
+        output reg [SYS_DWIDTH - 1 : 0]     data0_o,    // Caesar Dec. Input
+        output reg                          valid0_o,   // Caesar Dec. Enable
+        
+        output reg [SYS_DWIDTH - 1 : 0]     data1_o,    // Scytale Dec. Output
+        output reg                          valid1_o,   // Scytale Dec. Enable
+        
+        output reg [SYS_DWIDTH - 1 : 0]     data2_o,    // ZigZag Dec. Output
+        output reg                          valid2_o    // ZigZag Dec. Enable
+    );
 	/////////////////////////// LOGIC OVERVIEW ///////////////////////////
 	//    On the positive edge of [master_clock]:                       //
 	//        if the input is enabled ([valid_i] is HIGH):              //
@@ -66,30 +69,74 @@ module demux #(
 	//            make sure to only do this for 4 clock cycles          //
 	//////////////////////////////////////////////////////////////////////
 
-	reg [MST_DWIDTH - 1 : 0] stored_data = 0;
-	reg [3:0] packet_counter = 0;
+    reg [MST_DWIDTH - 1 : 0] stored_data = 0;
+    reg [1:0] state = 2'b00;
 
-	always @(posedge clk_mst) begin
-		stored_data <= (valid_i) ? data_i : 0;
-	end
+    always @(posedge clk_sys) begin
+        if (rst_n) begin // if(!reset)
+            case (state) // fsm start
+                2'b00: begin // State 1: print 3rd character
+                    data0_o  = (select == 2'b00) ? stored_data[23:16] : 0;
+                    valid0_o = (select == 2'b00) ? 1 : 0;
 
-	always @(posedge clk_sys) begin
-		if (stored_data != 0) begin
-			data0_o  = (select == 2'b00) ? stored_data[SYS_DWIDTH * (3 - packet_counter) +: SYS_DWIDTH] : 0;
-			valid0_o = (select == 2'b00) ? 1 : 0;
+                    data1_o  = (select == 2'b01) ? stored_data[23:16] : 0;
+                    valid1_o = (select == 2'b01) ? 1 : 0;
 
-			data1_o  = (select == 2'b01) ? stored_data[SYS_DWIDTH * (3 - packet_counter) +: SYS_DWIDTH] : 0;
-			valid1_o = (select == 2'b01) ? 1 : 0;
+                    data2_o  = (select == 2'b10) ? stored_data[23:16] : 0;
+                    valid2_o = (select == 2'b10) ? 1 : 0;
 
-			data2_o  = (select == 2'b10) ? stored_data[SYS_DWIDTH * (3 - packet_counter) +: SYS_DWIDTH] : 0;
-			valid2_o = (select == 2'b10) ? 1 : 0;
+                    // if we are receiveing an input or if we haven't finished
+                    // outputting the previous one
+                    if (valid_i || stored_data != 0)
+                        state <= state + 2'b01; // go to next state
+                end
 
-			packet_counter <= (packet_counter == 3) ? 0 : packet_counter + 1;
-		end else begin
-			data0_o  = 0; valid0_o = 0;
-			data1_o  = 0; valid1_o = 0;
-			data2_o  = 0; valid2_o = 0;
-			packet_counter <= 0;
-		end
-	end
+                2'b01: begin // State 2: print 2nd character
+                    data0_o  = (select == 2'b00) ? stored_data[15:8] : 0;
+                    valid0_o = (select == 2'b00) ? 1 : 0;
+
+                    data1_o  = (select == 2'b01) ? stored_data[15:8] : 0;
+                    valid1_o = (select == 2'b01) ? 1 : 0;
+
+                    data2_o  = (select == 2'b10) ? stored_data[15:8] : 0;
+                    valid2_o = (select == 2'b10) ? 1 : 0;
+
+                    state <= state + 2'b01; // go to next state
+                end
+                
+                2'b10: begin // State 3: print 1st character & read input
+                    data0_o  = (select == 2'b00) ? stored_data[7:0] : 0;
+                    valid0_o = (select == 2'b00) ? 1 : 0;
+
+                    data1_o  = (select == 2'b01) ? stored_data[7:0] : 0;
+                    valid1_o = (select == 2'b01) ? 1 : 0;
+
+                    data2_o  = (select == 2'b10) ? stored_data[7:0] : 0;
+                    valid2_o = (select == 2'b10) ? 1 : 0;
+
+                    stored_data <= (valid_i) ? data_i : 0; // store input in reg
+                    
+                    state <= state + 2'b01; // go to next state
+                end
+                
+                2'b11: begin // State 4: print 4th character
+                    data0_o  = (select == 2'b00) ? stored_data[31:24] : 0;
+                    valid0_o = (select == 2'b00) ? 1 : 0;
+
+                    data1_o  = (select == 2'b01) ? stored_data[31:24] : 0;
+                    valid1_o = (select == 2'b01) ? 1 : 0;
+
+                    data2_o  = (select == 2'b10) ? stored_data[31:24] : 0;
+                    valid2_o = (select == 2'b10) ? 1 : 0;
+                    
+                    state <= state + 2'b01; // go to first state (overflow 2bit reg)
+                end
+            endcase
+        end else begin // if (reset)
+            data0_o  = 0; valid0_o = 0;
+            data1_o  = 0; valid1_o = 0;
+            data2_o  = 0; valid2_o = 0;
+            state <= 0; stored_data <= 0;
+        end
+    end
 endmodule
